94e3797c3a2a9d257203ba218b382641a8070953
Shmuel Lotman <shmuel.lotman@gmail.com>
2017-11-21 11:38:54 -0500

    initial implementation trip planner

---
+++ src/marker.js
@@ -0,0 +1,22 @@
+const mapboxgl = require("mapbox-gl");
+function addMarker(type, long, lat) {
+    let markerDomEl = document.createElement("div");
+    markerDomEl.style.width = "32px";
+    markerDomEl.style.height = "39px";
+    if(type === 'activity') {
+        markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+    } else if(type === 'hotel') {
+        markerDomEl.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)";
+    } else if(type === 'restaurant') {
+        markerDomEl.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
+    }
+     return new mapboxgl.Marker(markerDomEl).setLngLat([long, lat]);
+}
+
+
+module.exports = addMarker
+
+
+
+
+
